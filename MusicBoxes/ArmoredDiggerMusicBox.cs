b68f25fdc372db862d0929ce0b2b3fdcb24a6c51
WitherTaco <sharonovmisha2005@outlook.com>
2023-12-11 07:55:31 +0300

    plaguebringer music box

--- MusicBoxes/ArmoredDiggerMusicBox.cs
+++ MusicBoxes/ArmoredDiggerMusicBox.cs
@@ -10,7 +10,7 @@ namespace ClamityMusic.MusicBoxes
 {
     public class ArmoredDiggerMusicBox : MusicBox
     {
-        public override int MusicBoxTile => ModContent.TileType<AcidRainT2MusicBoxTile>();
+        public override int MusicBoxTile => ModContent.TileType<ArmoredDiggerMusicBoxTile>();
         public override string MusicName => "ArmoredDigger";
     }
     public class ArmoredDiggerMusicBoxTile : MusicBoxTile
