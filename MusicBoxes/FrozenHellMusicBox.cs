9c56c6fc51e6ccb99557fece204e17406e5e9d22
WitherTaco2 <133594341+WitherTaco2@users.noreply.github.com>
2024-01-31 07:41:53 +0300

    frozen hell theme

---
+++ MusicBoxes/FrozenHellMusicBox.cs
@@ -0,0 +1,17 @@
+﻿using Terraria.ModLoader;
+
+namespace ClamityMusic.MusicBoxes
+{
+    public class FrozenHellMusicBox : MusicBox
+    {
+        public override bool IsLoadingEnabled(Mod mod)
+        {
+            return ModLoader.GetMod("Clamity") != null;
+        }
+        public override int MusicBoxTile => ModContent.TileType<FrozenHellMusicBoxTile>();
+        public override string MusicName => "FrozenHell";
+    }
+    public class FrozenHellMusicBoxTile : MusicBoxTile
+    {
+    }
+}
